2bf4670c1ab8216dc63261671ab5f3afa7febbf8
Maurilio Medeiros <maurilioalm@hotmail.com>
2020-10-01 13:33:14 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/maurilioalm/workshop/resources/UserResource.java
+++ src/main/java/com/maurilioalm/workshop/resources/UserResource.java
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.maurilioalm.workshop.domain.Post;
 import com.maurilioalm.workshop.domain.User;
 import com.maurilioalm.workshop.dto.UserDTO;
 import com.maurilioalm.workshop.services.UserService;
@@ -61,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
