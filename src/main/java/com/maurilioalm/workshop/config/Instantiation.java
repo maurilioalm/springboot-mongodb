99120ef6833f791cf714b16ed55f5ff45d355919
Maurilio Medeiros <maurilioalm@hotmail.com>
2020-10-01 13:09:39 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/maurilioalm/workshop/config/Instantiation.java
+++ src/main/java/com/maurilioalm/workshop/config/Instantiation.java
@@ -1,6 +1,5 @@
 package com.maurilioalm.workshop.config;
 
-
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -11,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.maurilioalm.workshop.domain.Post;
 import com.maurilioalm.workshop.domain.User;
+import com.maurilioalm.workshop.dto.AuthorDTO;
 import com.maurilioalm.workshop.repository.PostRepository;
 import com.maurilioalm.workshop.repository.UserRepository;
 
@@ -19,28 +19,30 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		userRepository.deleteAll();
-		
+		postRepository.deleteAll();
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("23/03/2018"), "Partiu viagem",  "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("23/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
+
 	}
 
 }
